56b8f102e1e7540431d0bcec2f5a1e59130cdbf1
Pablo <guinazuzu@gmail.com>
2024-06-06 21:46:59 -0400

    Se integra react, ts. Se crea el componente buttonSesion con react  donde esta el formulario login , junto con el formulario de registrar usuario, se adapta el navbar y el layout para los nuevos compónentes

--- src/components/ButtonSesion.jsx
+++ src/components/ButtonSesion.jsx
@@ -1,60 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Registrar from './Registrar';
 
-export const ButtonSesion = () => {
+const ButtonSesion = () => {
+  const [isShowLogin, setIsShowLogin] = useState(false);
+	const [issetShowRegister, setShowRegister] = useState(false);
+
+
+  const openLogin = () => {
+    setIsShowLogin(true);
+		setShowRegister(false);
+  };
+
+	const openRegister = () => {
+    setIsShowLogin(false);
+    setShowRegister(true);
+  };
+
+  const closeModulo = () => {
+    setIsShowLogin(false);
+		setShowRegister(false);
+  };
 
   return (
     <>
-			{/* <!-- Modal toggle --> */}
-			<button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-				Toggle modal
-			</button>
 
-			{/* <!-- Main modal --> */}
-			<div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-					<div class="relative p-4 w-full max-w-md max-h-full">
-							{/* <!-- Modal content --> */}
-							<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-									{/* <!-- Modal header --> */}
-									<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-											<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-													Sign in to our platform
-											</h3>
-											<button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-													<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-															<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-													</svg>
-													<span class="sr-only">Close modal</span>
-											</button>
-									</div>
-									{/* <!-- Modal body --> */}
-									<div class="p-4 md:p-5">
-											<form class="space-y-4" action="#">
-													<div>
-															<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-															<input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
-													</div>
-													<div>
-															<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-															<input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-													</div>
-													<div class="flex justify-between">
-															<div class="flex items-start">
-																	<div class="flex items-center h-5">
-																			<input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-																	</div>
-																	<label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-															</div>
-															<a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-													</div>
-													<button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-													<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-															Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-													</div>
-											</form>
-									</div>
-							</div>
-					</div>
-			</div>
+      <button
+        onClick={openLogin}
+        className="block text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+        type="button"
+      >
+        Inicio Sesión
+      </button>
+
+      {isShowLogin && (
+        <div
+          id="authentication-modal"
+          tabIndex="-1"
+          aria-hidden="true"
+          className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50"
+        >
+          <div className="relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Iniciar Sesión
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+                onClick={closeModulo}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Cerrar</span>
+              </button>
+            </div>
+            <div className="p-4 md:p-5">
+              <form className="space-y-4" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Correo
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Correo@email.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Contraseña
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    required
+                  />
+                </div>
+                <div className="flex justify-end">
+                  <a
+                    href="#"
+                    className="text-sm  hover:underline dark:text-white"
+                  >
+                    Recuperar Clave
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                >
+                  Ingresar
+                </button>
+								<div className="flex justify-end">
+									<button
+										type="button"
+										className="dark:text-white bg-transparent  hover:underline rounded-lg text-sm  flex items-center justify-center  "
+										onClick={openRegister}
+										>
+											Registrate
+									</button>
+								</div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {issetShowRegister && (
+        <Registrar closeModulo={closeModulo} />
+      )}
     </>
   );
 };
+
+export default ButtonSesion;
